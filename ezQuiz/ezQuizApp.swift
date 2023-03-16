6b79dd7a9b5a403a172f84c288407cd7d7844d94
Ruslan Sadritdinov <rus9606@icloud.com>
2023-03-17 01:31:03 +0300

    Initial Commit

---
+++ ezQuiz/ezQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ezQuizApp.swift
+//  ezQuiz
+//
+//  Created by Ruslan Sadritdinov on 17.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ezQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
