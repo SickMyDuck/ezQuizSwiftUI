356060384a623c6a542813ee9e7de6bb67361211
Ruslan Sadritdinov <rus9606@icloud.com>
2023-04-08 12:48:16 +0300

    Add flags router

---
+++ ezQuiz/Flags/Router/FlagsRouter.swift
@@ -0,0 +1,16 @@
+//
+//  FlagsRouter.swift
+//  ezQuiz
+//
+//  Created by Ruslan Sadritdinov on 08.04.2023.
+//
+
+import UIKit
+
+protocol FlagsRouterProtocol {
+
+}
+
+class FlagsRouter: FlagsRouterProtocol {
+
+}
