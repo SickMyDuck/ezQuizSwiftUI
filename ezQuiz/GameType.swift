2bcf12b16c153084a6d41c965b940b85c545c4a6
Ruslan Sadritdinov <rus9606@icloud.com>
2023-04-30 12:24:40 +0300

    Add GameType

---
+++ ezQuiz/GameType.swift
@@ -0,0 +1,13 @@
+//
+//  GameType.swift
+//  ezQuiz
+//
+//  Created by Ruslan Sadritdinov on 29.04.2023.
+//
+
+import Foundation
+
+enum GameType: String, CaseIterable {
+    case flagGame
+    case countryGame
+}
